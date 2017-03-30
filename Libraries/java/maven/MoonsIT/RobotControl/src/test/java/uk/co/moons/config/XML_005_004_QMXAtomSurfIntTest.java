a80b64d354b060d6bb020bf70e3920aca2a96e2c
Rupert Young <rupert@moonsit.co.uk>
2017-03-30 18:52:21 +0100

    u

---
+++ Libraries/java/maven/MoonsIT/RobotControl/src/test/java/uk/co/moons/config/XML_005_004_QMXAtomSurfIntTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.co.moons.config;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ReStart
+ */
+public class XML_005_005_XPVAControlTest {
+    
+    public XML_005_005_XPVAControlTest() {
+    }
+
+    /**
+     * Test of main method, of class TestMain.
+     */
+    @Test
+    public void testMain() throws Exception {
+        String[] args = {"..\\..\\..\\..\\..\\Controllers\\Models\\QuantumMoves\\", "005-005-XPVAControl"};
+        System.out.println(args[0]+args[1]);
+
+
+        try {
+            ODGConfig.main(args);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println(ex.toString());
+            System.out.println(ex.getCause());
+            fail(ex.toString());
+        }
+    }
+    
+}
